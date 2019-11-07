904df16a3276b5cfedbf235fe3c95cf0e7266741
Jose J Jimenez <jjjimenezasensio@icloud.com>
2019-11-07 07:33:07 +0100

    Inicio del proyecto

---
+++ NERiOS/NERiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NERiOS
+//
+//  Created by José Jiménez Asensio on 07/11/2019.
+//  Copyright © 2019 KapuaStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
